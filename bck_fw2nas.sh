d9e08250d859923e8ca574fcb71591d71338b193
dejavudf <69731110+dejavudf@users.noreply.github.com>
2020-08-17 18:35:28 -0300

    Update and rename bck_fw2nas.sh to remote_file_cfg_backup.sh

--- bck_fw2nas.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#copia arquivos para servidor de arquivos windows no azure
-cd /backup_fw/data
-sudo smbclient -U ihb.local/hepta.alexsandro.f //10.100.0.10/Backup-Servicos/ -c 'prompt ; cd /FIREWALLS ; mput *201905*'
