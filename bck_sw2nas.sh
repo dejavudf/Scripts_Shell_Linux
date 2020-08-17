cca79339f6d974bb41d685dcf576cdb7987bfe67
dejavudf <69731110+dejavudf@users.noreply.github.com>
2020-08-17 18:43:53 -0300

    Delete bck_sw2nas.sh

--- bck_sw2nas.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#copia arquivos para servidor de arquivos windows no azure
-cd /backup_sw/data
-sudo smbclient -U ihb.local/hepta.alexsandro.f //10.100.0.10/Backup-Servicos/ -c 'prompt ; cd /SWITCHES ; mput *201905*'
-
