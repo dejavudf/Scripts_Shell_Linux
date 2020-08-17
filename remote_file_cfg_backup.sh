88f5d6df9b0b54fea2cdaa394260c0a47bea3112
dejavudf <69731110+dejavudf@users.noreply.github.com>
2020-08-17 18:39:29 -0300

    Update remote_file_cfg_backup.sh

--- remote_file_cfg_backup.sh
+++ remote_file_cfg_backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#copia arquivos para servidor de arquivos windows no azure
+#copia arquivos para servidor de arquivos windows
 cd /backup_fw/data
-sudo smbclient -U domain/user //10.100.0.10/Backup-Servicos/ -c 'prompt ; cd /FIREWALLS ; mput *201905*'
+sudo smbclient -U domain/user //ip_hostname/Backup-Servicos/ -c 'prompt ; cd /FIREWALLS ; mput *201905*'
